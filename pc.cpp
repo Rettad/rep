4129e19933b0c4a26241d8758c726ab5d61edb8f
Roman Zabara <r.zabara22@mail.ru>
2024-12-24 17:03:14 +0300

    rrrr

--- pc.cpp
+++ pc.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main() {
-    int a = 11, b = 9;
-    // Добавляем два числа и выводим их сумму
-    cout << a + b;
+    int a = 11, b = 9, c = 22;
+
+    cout << a + b + c;
     return 0;
 }
